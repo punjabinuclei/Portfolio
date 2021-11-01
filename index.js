f7a308163da359efff1e52d227df3403c51b1962
Gurpreet Singh <62651343+guruXD0204@users.noreply.github.com>
2021-11-01 16:28:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// ANIMATIONS
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
+tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
+tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
+tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
+tl.fromTo(".headings", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+tl.fromTo(".icon-link", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
